96bee22e9634608c84b7224410373f967b965e61
R-P-Anuraj <anurajchoukeri@gmail.com>
2025-08-01 12:19:05 +0530

    final

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,12 +1,26 @@
 import express from "express";
 import { response } from "../helper/callback";
-import { createUserController ,loginUserController,getUserController} from "../controllers/userController";
-import { userRegisterV ,userLoginV} from "../validators";
+import {
+  createUserController,
+  loginUserController,
+  getUserController,
+} from "../controllers/userController";
+import { userRegisterV, userLoginV } from "../validators";
 import { authUser } from "../middlewares/userMiddleware";
 const router = express.Router();
 
-router.post("/createUser",userRegisterV.userRegisterValidator, response(createUserController));
-router.post("/loginUser", userLoginV.userLoginValidator,response(loginUserController));
-router.get('/profile',authUser,response(getUserController))
+router.post(
+  "/createUser",
+  userRegisterV.userRegisterValidator,
+  response(createUserController)
+);
+
+router.post(
+  "/loginUser",
+  userLoginV.userLoginValidator,
+  response(loginUserController)
+);
+
+router.get("/profile", authUser, response(getUserController));
 
 export default router;
